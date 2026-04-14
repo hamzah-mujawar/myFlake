931fb89401d35e4c6fe4f429c2b3429d5281659d
Hamzah <hamzah1faisal@gmail.com>
2026-04-14 23:59:20 +0400

    wrapping helix not tmux

--- wrappedPrograms/helix.nix
+++ wrappedPrograms/helix.nix
@@ -3,7 +3,7 @@
 		environment.systemPackages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.myHelix ];
 	};
 	perSystem = { pkgs, lib, self', ... }: {
-		packages.myHelix = inputs.wrapper-modules.wrappers.tmux.wrap {
+		packages.myHelix = inputs.wrapper-modules.wrappers.helix.wrap {
 			inherit pkgs;
 		};
 	};
