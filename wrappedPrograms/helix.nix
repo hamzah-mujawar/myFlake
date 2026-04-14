109c3b92123f461dedc26145cc3db36973776033
Hamzah <hamzah1faisal@gmail.com>
2026-04-14 23:40:27 +0400

    added helix

---
+++ wrappedPrograms/helix.nix
@@ -0,0 +1,20 @@
+{ self, inputs, ... }:{
+	flake.nixosModules.helix = { pkgs, lib, ... }: {
+		programs.helix = {
+			enable = true;
+			package = self.packages.${pkgs.stdenv.hostPlatform.system}.myHelix;
+		};
+	};
+	perSystem = { pkgs, lib, self', ... }: {
+		packages.myHelix = inputs.wrapper-modules.wrappers.tmux.wrap {
+			inherit pkgs;
+			languages = {
+				name = "c++"
+				auto-format = false
+				language-server = { command = "ccls", args = [] }
+			};
+		};
+	};
+}
+
+
