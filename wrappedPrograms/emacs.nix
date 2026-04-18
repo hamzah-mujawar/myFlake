b170bb35da0ebb379b783dab90a6162669b72ec4
Hamzah <hamzah1faisal@gmail.com>
2026-04-19 00:56:15 +0400

    colored man pages

--- wrappedPrograms/emacs.nix
+++ wrappedPrograms/emacs.nix
@@ -1,64 +1,72 @@
-{ self, inputs, ... }:
 {
-  flake.nixosModules.emacs = { pkgs, lib, ... }: {
-    environment.systemPackages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.custom-emacs ];
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.emacs = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    environment.systemPackages = [self.packages.${pkgs.stdenv.hostPlatform.system}.custom-emacs];
     services.emacs.enable = true;
     services.emacs.package = self.packages.${pkgs.stdenv.hostPlatform.system}.custom-emacs;
   };
 
-  perSystem = { pkgs, ... }: let
-    newpkgs = pkgs.appendOverlays
+  perSystem = {pkgs, ...}: let
+    newpkgs =
+      pkgs.appendOverlays
       (with inputs.emacs-overlay.overlays; [
         emacs
         package
       ]);
 
-    tree-sitter-parsers = grammars: with grammars; [
-      tree-sitter-bash
-      tree-sitter-c
-      tree-sitter-c-sharp
-      tree-sitter-cmake
-      tree-sitter-cpp
-      tree-sitter-css
-      tree-sitter-dot
-      tree-sitter-elisp
-      tree-sitter-glsl
-      tree-sitter-haskell
-      tree-sitter-html
-      tree-sitter-java
-      tree-sitter-javascript
-      tree-sitter-json
-      tree-sitter-json5
-      tree-sitter-kotlin
-      tree-sitter-latex
-      tree-sitter-llvm
-      tree-sitter-lua
-      tree-sitter-make
-      tree-sitter-markdown
-      tree-sitter-markdown-inline
-      tree-sitter-nickel
-      tree-sitter-nix
-      tree-sitter-prisma
-      tree-sitter-python
-      tree-sitter-regex
-      tree-sitter-rust
-      tree-sitter-scss
-      tree-sitter-sql
-      tree-sitter-toml
-      tree-sitter-tsx
-      tree-sitter-typescript
-      tree-sitter-vim
-      tree-sitter-yaml
-      tree-sitter-zig ];
+    tree-sitter-parsers = grammars:
+      with grammars; [
+        tree-sitter-bash
+        tree-sitter-c
+        tree-sitter-c-sharp
+        tree-sitter-cmake
+        tree-sitter-cpp
+        tree-sitter-css
+        tree-sitter-dot
+        tree-sitter-elisp
+        tree-sitter-glsl
+        tree-sitter-haskell
+        tree-sitter-html
+        tree-sitter-java
+        tree-sitter-javascript
+        tree-sitter-json
+        tree-sitter-json5
+        tree-sitter-kotlin
+        tree-sitter-latex
+        tree-sitter-llvm
+        tree-sitter-lua
+        tree-sitter-make
+        tree-sitter-markdown
+        tree-sitter-markdown-inline
+        tree-sitter-nickel
+        tree-sitter-nix
+        tree-sitter-prisma
+        tree-sitter-python
+        tree-sitter-regex
+        tree-sitter-rust
+        tree-sitter-scss
+        tree-sitter-sql
+        tree-sitter-toml
+        tree-sitter-tsx
+        tree-sitter-typescript
+        tree-sitter-vim
+        tree-sitter-yaml
+        tree-sitter-zig
+      ];
 
-      custom-emacs =
-      with newpkgs;
-      ((emacsPackagesFor (emacs30-pgtk.override { withNativeCompilation = true; })).emacsWithPackages (epkgs: with epkgs; [
+    custom-emacs = with newpkgs; ((emacsPackagesFor (emacs30-pgtk.override {withNativeCompilation = true;})).emacsWithPackages (epkgs:
+      with epkgs; [
         jinx
         vterm
         (treesit-grammars.with-grammars (grammars: tree-sitter-parsers grammars))
       ]));
-
   in {
     packages = {
       inherit custom-emacs;
--- wrappedPrograms/environment.nix
+++ wrappedPrograms/environment.nix
@@ -1,46 +1,47 @@
 {
- lib,
- inputs,
- self,
- ...
+  lib,
+  inputs,
+  self,
+  ...
 }: {
-	perSystem = {
-	    pkgs,
-	    self',
-	    ...
-	  }: {
+  perSystem = {
+    pkgs,
+    self',
+    ...
+  }: {
+    # My primary flake shell with all of it's packages
+    packages.environment = inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = self'.packages.fish;
+      runtimeInputs = [
+        # nix
+        pkgs.nil
+        pkgs.nixd
+        pkgs.statix
+        pkgs.alejandra
+        pkgs.manix
+        pkgs.nix-inspect
+        self'.packages.nh
 
-	    # My primary flake shell with all of it's packages
-	    packages.environment = inputs.wrappers.lib.wrapPackage {
-	      inherit pkgs;
-	      package = self'.packages.fish;
-	      runtimeInputs = [
-		# nix
-		pkgs.nil
-		pkgs.nixd
-		pkgs.statix
-		pkgs.alejandra
-		pkgs.manix
-		pkgs.nix-inspect
-		self'.packages.nh
+        # other
+        pkgs.unzip
+        pkgs.zip
+        pkgs.wget
+        pkgs.fzf
+        pkgs.bottom
+        pkgs.fd
+        pkgs.zoxide
+        pkgs.ripgrep
+        pkgs.fastfetch
+        pkgs.wl-clipboard
+        pkgs.gdb
 
-		# other
-		pkgs.unzip
-		pkgs.zip
-		pkgs.wget
-		pkgs.fzf
-		pkgs.bottom
-		pkgs.fd
-		pkgs.zoxide
-		pkgs.ripgrep
-		pkgs.fastfetch
-		pkgs.wl-clipboard
-		pkgs.gdb
+        #wrapped
+        self'.packages.git
 
-		#wrapped
-		self'.packages.git
-	      ];
-	    };
-
-	};
+        #fish plugins
+        pkgs.fishPlugins.colored-man-pages
+      ];
+    };
+  };
 }
